05bda45cfec3b6409fe94a6e170e3ba0455fc3f8
Alexander Lemport <lemportpro@mail.ru>
2023-11-19 16:23:06 +0300

    Added to lists for robots and humans with 1 and 0

--- python_intro/lesson_10/whoami.py
+++ python_intro/lesson_10/whoami.py
@@ -3,7 +3,15 @@ import random
 lst = ['robot'] * 10
 lst += ['human'] * 10
 random.shuffle(lst)
-data = pd.DataFrame({'whoAmI':lst})
-data.head()
+robots = [1 if who == "robot" else 0 for who in lst]
+humans = [1 if who == "human" else 0 for who in lst]
 
-pd.get_dummies(data['whoAmI'])
+print(robots)
+print(humans)
+
+# data = pd.DataFrame({'whoAmI':lst})
+# data.head(20)
+
+
+
+#pd.get_dummies(data['whoAmI'])
