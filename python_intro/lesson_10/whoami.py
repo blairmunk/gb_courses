6a075b8432f234e25ab595e74ac90c58464e7242
Alexander Lemport <lemportpro@mail.ru>
2023-11-19 16:33:44 +0300

    Added dataframe with dict. get_dummies method was commented

--- python_intro/lesson_10/whoami.py
+++ python_intro/lesson_10/whoami.py
@@ -6,12 +6,9 @@ random.shuffle(lst)
 robots = [1 if who == "robot" else 0 for who in lst]
 humans = [1 if who == "human" else 0 for who in lst]
 
-print(robots)
-print(humans)
+data = pd.DataFrame({'human': humans, 'robot': robots})
+data.head(20)
 
 # data = pd.DataFrame({'whoAmI':lst})
+# pd.get_dummies(data['whoAmI'])
 # data.head(20)
-
-
-
-#pd.get_dummies(data['whoAmI'])
